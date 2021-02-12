e1c6ea4b577887a5394394d3212644b61cc3942a
John <jgilpn@gmail.com>
2021-02-11 16:12:38 -0800

    Added Timer component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css'
+import Timer from './components/Timer'
 
 function App() {
-  return <div className='app-container'></div>
+  return (
+    <div className='app-container'>
+      <Timer />
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/Timer.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+// Timer Functions
+const startTimer = () => {
+  console.log('Starting timer.')
+}
+
+const stopTimer = () => {
+  console.log('Stopping timer.')
+}
+
+const resetTimer = () => {
+  console.log('Resetting timer.')
+}
+
+// Timer Component
+const Timer = () => {
+  return <div className='timer-container'></div>
+}
+
+export default Timer
